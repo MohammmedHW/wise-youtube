82d8cf601eb51489172e254ea3a34a38158bcca5
MohammedHW <hoshsangabadwalam@gmail.com>
2025-08-25 17:25:23 +0530

    My changes to config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ export const config = {
   cli: {
     url: 'http://timesride.com/custom',
     base_url: 'https://youtube.googleapis.com/youtube/v3',
-    api_key: 'AIzaSyAt0lZj0hUePz1K3KjLFTP_If3mZdXiGlw',
+    api_key: 'AIzaSyDuxn6LwlPO7NHK0phGRjyMqDHbJxmuftQ',
   },
 };
